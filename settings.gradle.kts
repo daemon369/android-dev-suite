968ad705f7f5b0342b49a6cb145e6c82f3059f25
daemon369 <daemon336699@gmail.com>
2022-03-02 20:31:47 +0800

    use android-version-catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
 rootProject.name =  "android-dev-suite"
 include(":app")
+
+dependencyResolutionManagement {
+    repositories {
+//        mavenLocal()
+        mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/releases/") }
+    }
+    versionCatalogs {
+        create("libs") {
+            from("io.github.daemon369:android-version-catalog:1.0.0")
+        }
+    }
+}
